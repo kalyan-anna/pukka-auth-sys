801ef76bec1b56eeedff23c1a86585ed434620de
Kalyan <kalyan@3wks.com.au>
2016-10-31 09:35:41 +1100

    Add HTML formatting

--- src/test/java/feature/login/LoginRunner.java
+++ src/test/java/feature/login/LoginRunner.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"pretty"} )
+@CucumberOptions(format = {"pretty", "html:build/reports/bdd/login"} )
 public class LoginRunner {
 }
